fef1db0e6aa7ed5d133c48c95985275e0ff935f6
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-30 22:18:10 -0500

    commit

--- server/api/v1/visitors/controller.js
+++ server/api/v1/visitors/controller.js
@@ -5,7 +5,7 @@ exports.createVisitor = async (req, res, next) => {
     const { body } = req;
     const visitorModel = new Visitor(body);
     const createdVisitorDoc = await visitorModel.save();
-    res.status(201).json(createdVisitorDoc);
+    res.status(201).send(`<h1>El visitante fue almacenado con éxito</h1>`);
   } catch (err) {
     next(err);
   }
